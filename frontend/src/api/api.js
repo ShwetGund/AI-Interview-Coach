50a825f4eebea6dbd0b7ff0d015368ef3d5886cf
Shweta Gund <your-email@gmail.com>
2026-06-03 17:19:44 +0530

    Connected Render backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+//const API_BASE_URL = "http://127.0.0.1:8000";
+
+const API_BASE_URL = "https://ai-interview-coach-049x.onrender.com";
 
 export const uploadResume = async (formData) => {
 
