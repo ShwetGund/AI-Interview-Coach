56d14694229d02dd78e52cf529fed4771141cf16
Shweta Gund <your-email@gmail.com>
2026-06-08 10:32:28 +0530

    fix interview cors

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.interview_routes import router as interview_router
 
 from routes.upload_routes import router as upload_router
+from routes.interview_routes import router as interview_router
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-interview-coach-self-ten.vercel.app",
+        "https://ai-interview-coach-bmdss0r2e-shweta-gund-s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,15 +24,3 @@ app.include_router(interview_router)
 @app.get("/")
 def home():
     return {"message": "Backend Running Successfully"}
-
-from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://ai-interview-coach-self-ten.vercel.app"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
